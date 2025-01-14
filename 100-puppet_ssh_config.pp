6812ec53c375af0eea9516a8ed1bca6dfc16465c
Sayialel <leninasayialel@gmail.com>
2025-01-14 11:43:09 -0800

    Last commit

---
+++ 100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+[200~#!/usr/bin/env bash
+# Automating my Tasks using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication lenina,
+}
